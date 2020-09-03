73213d8b83d8afac0c0146fc7868e9f163c80f95
mina0774 <mina0774@hanmail.net>
2020-09-03 22:24:39 +0900

    character level screen!

--- lib/main.dart
+++ lib/main.dart
@@ -6,31 +6,132 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'First app',
-      home: MyHomePage(),
+      debugShowCheckedModeBanner: false, // 디버그 띠 없애기
+      title: 'MERIEL',
+      home: Grade(),
     );
   }
 }
 
-class MyHomePage extends StatelessWidget {
+class Grade extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.amber[800],
       appBar: AppBar(
         title: Text('MERIEL'),
         centerTitle: true,
-        backgroundColor: Colors.redAccent,
+        backgroundColor: Colors.amber[700],
         elevation: 0.0,
       ),
-      body: Center( // 가로 센터
+      body: Padding(
+        padding: EdgeInsets.fromLTRB(30.0, 40.0, 0.0, 0.0),
         child: Column(
-            mainAxisAlignment: MainAxisAlignment.center, //세로 센터
-            children: <Widget>[
-              Text('Hello'),
-              Text('Hello'),
-              Text('Hello')
-            ],
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: <Widget>[
+            Center(
+              child: CircleAvatar(
+                backgroundImage: AssetImage('assets/snoopy_gif.gif'),
+                radius: 60.0,
+              ),
+            ),
+            Divider(
+              height: 60.0,
+              color: Colors.white,
+              thickness: 0.5,
+              endIndent: 30.0, // 디바이더 속성이 끝에서부터 얼마나 떨어져 있어야하는
+            ),
+            Text('NAME',
+          style: TextStyle(
+            color: Colors.white,
+            letterSpacing: 2.0
           ),
+            ),
+            SizedBox(
+              height: 10.0,
+            ),
+            Text('MERIEL',
+              style: TextStyle(
+                color: Colors.white,
+                letterSpacing: 2.0,
+                fontSize: 28.0,
+                fontWeight: FontWeight.bold
+              ),
+            ),
+            SizedBox(
+              height: 30.0,
+            ),
+            Text('MERIEL POWER LEVEL',
+              style: TextStyle(
+                  color: Colors.white,
+                  letterSpacing: 2.0
+              ),
+            ),
+            SizedBox(
+              height: 10.0,
+            ),
+            Text('20',
+              style: TextStyle(
+                  color: Colors.white,
+                  letterSpacing: 2.0,
+                  fontSize: 28.0,
+                  fontWeight: FontWeight.bold
+              ),
+            ),
+            SizedBox(
+              height: 30.0,
+            ),
+            Row( //가로 배열
+              children: <Widget>[
+                Icon(Icons.check_circle_outline),
+                SizedBox(
+                  width: 10.0,
+                ),
+                Text('using lightsaber',
+                style: TextStyle(
+                  fontSize: 16.0,
+                  letterSpacing: 1.0
+                ),
+                ),
+              ],
+            ),
+            Row( //가로 배열
+              children: <Widget>[
+                Icon(Icons.check_circle_outline),
+                SizedBox(
+                  width: 10.0,
+                ),
+                Text('face hero tatoo',
+                  style: TextStyle(
+                      fontSize: 16.0,
+                      letterSpacing: 1.0
+                  ),
+                ),
+              ],
+            ),
+            Row( //가로 배열
+              children: <Widget>[
+                Icon(Icons.check_circle_outline),
+                SizedBox(
+                  width: 10.0,
+                ),
+                Text('fire flames',
+                  style: TextStyle(
+                      fontSize: 16.0,
+                      letterSpacing: 1.0
+                  ),
+                ),
+              ],
+            ),
+            Center(
+              child: CircleAvatar(
+                backgroundImage: AssetImage('assets/snoopy.jpg'),
+                radius: 40.0,
+              ),
+            )
+
+      ],
+      ),
       ),
     );
   }
